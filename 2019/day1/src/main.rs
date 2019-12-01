3d2a2fbb854bbed9c2a0d0f25fecff3e86157f42
loara <loarabia@hotmail.com>
2019-11-30 23:36:32 -0800

    Slightly less repetitive.

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -6,34 +6,34 @@ fn main() {
     let filename = "input.txt";
     let input = read_to_string(filename).expect("Reading the file failed");
 
+    let module_masses :Vec<i32> = input.lines()
+        .map(|line| i32::from_str(line).expect("Bad line item")).collect();
+
     //Part 1
-    let fuel_mass:i32 = input.lines()
-            .map(|line| required_fuel(i32::from_str(line).expect("Bad line item")))
-            .sum();
-    
+    let fuel_mass:i32 = module_masses.iter()
+            .map( |mass| required_fuel(mass))
+            .sum();    
     println!("{}", fuel_mass);
     
     // Part 2
-    let module_mass:i32 = input.lines()
-            .map(|line| i32::from_str(line).expect("Bad line item"))
-            .sum();
+    let modules_mass:i32 = module_masses.iter().sum();
 
-    let launch_mass_plus_fuel:i32 = input.lines()
-            .map(|line| grossed_up_mass(i32::from_str(line).expect("Bad line item")))
-            .sum();
+    let launch_mass_plus_fuel:i32 = module_masses.iter().
+        map(|mass| grossed_up_mass(mass))
+        .sum();
     
-    println!("{}", launch_mass_plus_fuel - module_mass);
+    println!("{}", launch_mass_plus_fuel - modules_mass);
 }
 
 // Part 1
-fn required_fuel(mass: i32) -> i32 {
+fn required_fuel(mass: &i32) -> i32 {
     mass/3-2
 }
 
 // Part 2
-fn grossed_up_mass(mass: i32) -> i32 {
-    if mass <= 0 {
+fn grossed_up_mass(mass: &i32) -> i32 {
+    if *mass <= 0 {
         return 0;
     }
-    mass + grossed_up_mass(mass/3-2) 
+    mass + grossed_up_mass( &(mass/3-2) ) 
 }
