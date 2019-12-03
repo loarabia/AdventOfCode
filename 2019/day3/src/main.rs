b0974af950bd1f8172ba59159f3619861c25bb44
loara <loarabia@hotmail.com>
2019-12-03 12:05:08 -0800

    Day 3 + git ignore file.
    Silly mistake of the day implementing Eq and not Hash.
    Other Silly mistake of the day, returning other distances than the one
    asked for.

---
+++ 2019/day3/src/main.rs
@@ -0,0 +1,139 @@
+use std::fmt;
+use std::fs;
+use std::hash::{Hash, Hasher};
+use std::str::FromStr;
+
+use std::collections::HashSet;
+
+use regex::Regex;
+fn main() {
+    let filename = "input.txt";
+    let contents = fs::read_to_string(filename).expect("File access error");
+
+    let wires:Vec<&str> = contents.split('\n').collect();
+
+    let wire1_def:Vec<&str> = wires[0].split(',').collect();
+    let wire2_def:Vec<&str> = wires[1].split(',').collect();
+
+    let wire1_pts:Vec<Point> = read_wire(wire1_def);
+    let wire2_pts:Vec<Point> = read_wire(wire2_def);
+
+    let mut set1:HashSet<Point> = HashSet::new();
+    let mut set2:HashSet<Point> = HashSet::new();
+
+    for pt in wire1_pts {
+        set1.insert(pt);
+    }
+    
+    for pt in wire2_pts {
+        set2.insert(pt);
+    }
+
+    run_part1(&set1, &set2);
+    run_part2(&set1, &set2);
+}
+
+fn run_part1(wire1:&HashSet<Point>, wire2:&HashSet<Point>) {
+    let part1 = wire1.intersection(wire2)
+        .min_by_key(|pt| pt.city_distance())
+        .unwrap();
+    println!("pt1: {}", part1.city_distance());
+}
+
+fn run_part2(wire1:&HashSet<Point>, wire2:&HashSet<Point>){
+    let point = wire1.intersection(wire2)
+        .min_by_key(|pt| {
+            let w1pt = wire1.get(pt).unwrap();
+            let w2pt = wire2.get(pt).unwrap();
+            w1pt.dist + w2pt.dist
+        })
+        .unwrap();
+    println!("pt2: {}", wire1.get(point).unwrap().dist + wire2.get(point).unwrap().dist);
+}
+
+fn read_wire(wire_def:Vec<&str>) -> Vec<Point> {
+    let mut result = Vec::new();
+    let regex_set:Vec<Regex> = vec![
+        Regex::new(r"(U)([0-9]+)").unwrap(),
+        Regex::new(r"(D)([0-9]+)").unwrap(),
+        Regex::new(r"(R)([0-9]+)").unwrap(),
+        Regex::new(r"(L)([0-9]+)").unwrap(),
+    ];
+
+    let mut current:Point = Point{x:0,y:0, dist:0};
+    for def in wire_def {
+
+        // Assumption: should only match one given the leading character.
+        // Capture Group 0 is the whole text.
+        // Capture Group 1 is the letter. 
+        // Capture Group 2 is an integer.
+        if let Some(cap) = regex_set[0].captures(def) {
+            // Up
+            let length = i32::from_str(cap.get(2).unwrap().as_str()).unwrap();
+            result.append(&mut build_list(&mut current, length, |pt| pt.y+=1));
+
+        } else if let Some(cap) = regex_set[1].captures(def) {
+            // Down
+            let length = i32::from_str(cap.get(2).unwrap().as_str()).unwrap();
+            result.append( &mut build_list(&mut current, length, |pt| pt.y-=1));
+
+        } else if let Some(cap) = regex_set[2].captures(def) {
+            // Right
+            let length = i32::from_str(cap.get(2).unwrap().as_str()).unwrap();
+            result.append( &mut build_list(&mut current, length, |pt| pt.x+=1));
+
+        } else if let Some(cap) = regex_set[3].captures(def) {
+            // Left
+            let length = i32::from_str(cap.get(2).unwrap().as_str()).unwrap();
+            result.append( &mut build_list(&mut current, length, |pt| pt.x-=1));
+        } else {
+            panic!("HOW?!?");
+        }
+    }
+    result
+}
+
+fn build_list<F>(start:&mut Point, length:i32, func:F ) -> Vec<Point>  
+    where F: Fn(&mut Point) {
+    let mut result = Vec::new();
+    for _ in 0..length {
+        func(start);
+        start.dist += 1;
+        result.push(start.clone());
+    }
+    result
+}
+
+#[derive(Clone)]
+struct Point {
+    x:i32,
+    y:i32,
+    dist:i32,
+}
+
+impl Point {
+    fn city_distance(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Point {}
+
+impl Hash for Point{
+    fn hash<H:Hasher>(&self, state: &mut H){
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {},  {})", self.x, self.y, self.dist)
+    }
+}
