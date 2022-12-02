3a06e2616d0eca3041b996060068f7b2a744cb12
Larry Olson <loarabia@hotmail.com>
2022-12-01 18:33:16 -0800

    Cleaned up enough aoc01

--- 2022/aoc01/src/main.rs
+++ 2022/aoc01/src/main.rs
@@ -1,37 +1,31 @@
 use std::fs::{read_to_string};
-use std::str::FromStr;
 
 fn main() {
     let input = read_to_string("input.txt").expect("Couldn't read input.text");
     println!("{}", part1(&input));
     println!("{}", part2(&input));
-    println!("Hello, world!");
 }
 
-fn part1(input: &String) -> i32 {
+fn pack_to_cals(pack:&str) -> i32 {
+    pack
+        .lines()
+        .map( |cals| cals.parse::<i32>().unwrap())
+        .sum()
+}
+
+fn part1(input:&String) -> i32 {
     input
         .split("\r\n\r\n")
-        .map( |elf_pack| 
-            elf_pack
-                .lines()
-                .map( |cals| 
-                    cals.parse::<i32>().unwrap())
-                .sum())
+        .map(|elf_pack| pack_to_cals(elf_pack))
         .max().unwrap()
-} 
+}
+
 fn part2(input: &String) -> i32 {
-    let mut elf_data:Vec<i32> = 
-    input
-    .split("\r\n\r\n")
-    .map( |elf_pack| 
-        elf_pack
-            .lines()
-            .map( |cals| 
-                cals.parse::<i32>().unwrap())
-            .sum::<i32>()).collect();
+    let mut elf_data:Vec<i32> =
+        input
+            .split("\r\n\r\n")
+            .map(|elf_pack| pack_to_cals(elf_pack))
+            .collect();
     elf_data.sort();
-    let slice = &elf_data[elf_data.len()-3..elf_data.len()];
-    assert!(slice.len() == 3);
-    slice.iter().sum()
-    
+    elf_data[elf_data.len()-3..elf_data.len()].iter().sum()
 }
